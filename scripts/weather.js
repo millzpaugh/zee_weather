66ab6415f93833fe190a47bd30825edb3140e82c
millzpaugh <ann.millspaugh@gmail.com>
2014-02-23 15:10:20 -0500

    styling updates

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,10 +1,48 @@
 var allWeatherInfo; 
 
+var summerPhotos = ["images/summer/12.jpg", "images/summer/13.jpg", "images/summer/14.jpg", "images/summer/15.jpg","images/summer/16.jpg", "images/summer/17.jpg", "images/summer/18.jpg", "images/summer/19.jpg", "images/summer/20.jpg", "images/summer/21.jpg", "images/summer/22.jpg", "images/summer/23.jpg", "images/fring/24.jpg", "images/fring/25.jpg", "images/fring/26.jpg", "images/fring/27.jpg","images/fring/28.jpg", "images/fring/29.jpg", "images/fring/30.jpg", "images/fring/31.jpg", "images/fring/32.jpg", "images/fring/33.jpg"] 
+
+// Grabs Random Photo 
 function randomFrom(arr){
     var randomIndex = Math.floor(Math.random() * arr.length);
     return arr[randomIndex];
 }
- 
+
+// Pings open weather API 
+  function getWeather(location) {
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + location, 
+      type: "GET",
+      dataType: "json",
+      success: function(response){
+        allWeatherInfo = response;  
+        appendWeather(response);  
+      },
+    }); 
+  } 
+
+// Logs weather info 
+  function appendWeather() {
+    console.log(allWeatherInfo.main.temp)
+      degreesF = Math.round((allWeatherInfo.main.temp- 273) * 9/5 + 32);
+      degreesC =  Math.round((allWeatherInfo.main.temp- 273)); 
+      if(degreesF > 60){
+        changeBackground();        
+        temp =  $("<li class='temp'>" + "<span class='response'>No!</span><br> You're in " + "<span class='city'>" + allWeatherInfo.name + "." + "</span>" + "<br>It's " + degreesF + "°F / " + degreesC + "°C. " + "<br><span id='phrase'>But here's a hipster scarf...</span>" + "</li>");
+        $("#list").append(temp); 
+        $('body').css('background-image', 'url(../' + randomFrom(summerPhotos) + ")'");} 
+      else{
+        temp =  $("<li class='temp'>" + "<span class='response'>Yes!</span><br> You're in " + "<span class='city'>" + allWeatherInfo.name + "</span>" + "<br>It's " + degreesF + "°F / " + degreesC + "°C." + "</li>");
+        $("#list").append(temp); 
+      }  
+     }
+
+//Changes background if weather is too hot for a scarf 
+function changeBackground(){
+  $('body').css('background-image', "url('" + randomFrom(summerPhotos) + "')" );
+}
+
+// Window onload events 
 $(function(){   
 
   $("form").on("submit",function(e){
@@ -20,56 +58,6 @@ $(function(){
   });
 
 }); 
-
-
-  function getWeather(location) {
-    $.ajax({
-      url: "http://api.openweathermap.org/data/2.5/weather?q=" + location, 
-      type: "GET",
-      dataType: "json",
-      success: function(response){
-        allWeatherInfo = response;  
-        appendWeather(response);  
-      },
-    }); 
-  } 
-
-// Backgrounds 
-// var winter = [#39131C,#4D192F,#501B26], 
-// var springfall = [#3E6B24, #5EBB3E, #1C541E, #31822B], 
-// var summer = [#4D2060,#8A66CC,#8F47C2,#4E2B82],  
-
-var winterPhotos = ["images/winter/1.jpg", "images/winter/2.jpg", "images/winter/3.jpg", "images/winter/4.jpg","images/winter/5.jpg", "images/winter/6.jpg", "images/winter/7.jpg", "images/winter/8.jpg", "images/winter/9.jpg", "images/winter/10.jpg", "images/winter/11.jpg"]
-
-var summerPhotos = ["images/summer/12.jpg", "images/summer/13.jpg", "images/summer/14.jpg", "images/summer/15.jpg","images/summer/16.jpg", "images/summer/17.jpg", "images/summer/18.jpg", "images/summer/19.jpg", "images/summer/20.jpg", "images/summer/21.jpg", "images/summer/22.jpg", "images/summer/23.jpg"]
-
-  function appendWeather() {
-    console.log(allWeatherInfo.main.temp)
-      degreesF = Math.round((allWeatherInfo.main.temp- 273) * 9/5 + 32);
-      degreesC =  Math.round((allWeatherInfo.main.temp- 273)); 
-      if(degreesF > 60){
-        temp =  $("<li class='temp'>" + "<span class='response'>No!</span><br> You're in " + "<span class='city'>" + allWeatherInfo.name + "." + "</span>" + "<br>It's " + degreesF + "°F / " + degreesC + "°C. " + "</li>");
-        $("#list").append(temp); 
-        hipster =  $("<li class='hipster-pic'>" + "<h2>But here's a hipster scarf..." + "</h2></li>");  
-        $("#list").append(hipster); 
-        hipsterImg = $("<img class='scarf' src=" + randomFrom(summerPhotos) + ">")
-        $("li.hipster-pic").append(hipsterImg); 
-      } 
-      else{
-        temp =  $("<li class='temp'>" + "<span class='response'>Yes!</span><br> You're in " + "<span class='city'>" + allWeatherInfo.name + "</span>" + "<br>It's " + degreesF + "°F / " + degreesC + "°C." + "</li>");
-        $("#list").append(temp); 
-        hipster =  $("<li class='hipster-pic'>" + "<h2>But here's a hipster scarf..." + "</h2></li>");  
-        $("#list").append(hipster); 
-        hipsterImg = $("<img class='scarf' src=" + randomFrom(winterPhotos) + ">")
-        $("li.hipster-pic").append(hipsterImg); 
-      }  
-     }
-    // }
-
-
-
-
-
 
 
 
