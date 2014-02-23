553c9297fd95c9472819d66d8883a02250ea010a
millzpaugh <ann.millspaugh@gmail.com>
2014-02-22 19:36:37 -0500

    weather displays and skeleton styling

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,31 +1,8 @@
 var allWeatherInfo; 
 
-  function getWeather(location) {
-    $.ajax({
-      url: "http://api.openweathermap.org/data/2.5/weather?q=" + location, 
-      type: "GET",
-      dataType: "json",
-      success: function(response){
-        allWeatherInfo = response;  
-        appendWeather(response);  
-      },
-    }); 
-  }   
-
-  function appendWeather(allWeatherInfo) {
-    console.log(allWeatherInfo.main.temp)
-    if(allWeatherInfo.message = "Error: Not found city"){
-        alert("Please trying re-entering your city."); 
-    }
-    else{
-      degrees = Math.round((allWeatherInfo.main.temp- 273) * 9/5 + 32); 
-      temp =  $("<li>" + "The temperature in " + allWeatherInfo.name + " is " + degrees + " degrees Fahrenheit" + "</li>");
-      $("#list").append(temp);
-    }
-  }
-
-
 $(function(){ 
+  $('.what').addClass('animated FadeInRight');
+  $('.weather-header').addClass('animated slideInLeft')
 
   $("form").on("submit",function(e){
     e.preventDefault();
@@ -36,8 +13,50 @@ $(function(){
   $('button#add').click(function(){
     $("#list").empty(); 
     appendWeather(); 
+    $("#list").empty(); 
   });
 
-
 }); 
+
+  function getWeather(location) {
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + location, 
+      type: "GET",
+      dataType: "json",
+      success: function(response){
+        allWeatherInfo = response;  
+        appendWeather(response);  
+      },
+    }); 
+  } 
+
+// Backgrounds 
+// var winter = [#39131C,#4D192F,#501B26], 
+// var springfall = [#3E6B24, #5EBB3E, #1C541E, #31822B], 
+// var summer = [#4D2060,#8A66CC,#8F47C2,#4E2B82],  
+
+  function appendWeather() {
+    console.log(allWeatherInfo.main.temp)
+      degreesF = Math.round((allWeatherInfo.main.temp- 273) * 9/5 + 32);
+      degreesC =  Math.round((allWeatherInfo.main.temp- 273)); 
+      if(degreesF > 60){
+        temp =  $("<li class='temp'>" + "<span class='response'>No!</span><br> You're in " + "<span class='city'>" + allWeatherInfo.name + "." + "</span>" + "<br>It's " + degreesF + "°F / " + degreesC + "°C. " + "</li>");
+        $("#list").append(temp); 
+      } else{
+        temp =  $("<li class='temp'>" + "<span class='response'>Yes!</span><br> You're in " + "<span class='city'>" + allWeatherInfo.name + "</span>" + " and it's " + degreesF + "°F / " + degreesC + "°C " + "</li>");
+        $("#list").append(temp); 
+      }  
+     }
+    // }
+
+
+
+
+
+
+
+
+
+
+
 
